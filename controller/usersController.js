885632571c9b8e597e2827ddb5e57e9cd835d4a8
Ali Kashev <kasev21@gmail.com>
2022-01-14 22:01:57 +0100

    added comments + simplyfied the code

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,13 +1,16 @@
 import { v4 as uuidv4 } from 'uuid';
 uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
+// Users array = database;
 let users = [];
 
+// getUsers
 export const getUsers = (req, res) => {
     console.log(users);
     res.send(users);
 }
 
+//getOneUser
 export const getOneUser = (req, res) => {
     const { id } = req.params;
 
@@ -15,12 +18,14 @@ export const getOneUser = (req, res) => {
     res.send(foundUser);
 }
 
+// createUser
 export const createUser = (req, res) => {
     const user = req.body;
     users.push({ ...user, id: uuidv4() });
     res.send(`user with name ${user.firstName} added to the database`);
 }
 
+// deleteUser
 export const deleteUser = (req, res) => {
     const { id } = req.params;
 
@@ -28,6 +33,7 @@ export const deleteUser = (req, res) => {
     res.send(`User with the ID: ${id} deleted from thet database.`);
 }
 
+// updateUser
 export const updateUser = (req, res) => {
     const { id } = req.params;
     const {firstName, lastName, age} = req.body
@@ -37,6 +43,5 @@ export const updateUser = (req, res) => {
     if (lastName) user.lastName = lastName;
     if (age) user.age = age;
     
-
     res.send(`User is updated`);
 }
--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,20 @@ import { getUsers, createUser, getOneUser, deleteUser, updateUser } from "../con
 const router = express.Router();
 
 // all routes will start with /users
+
+// Read All
 router.get('/', getUsers)
 
+// Create
 router.post('/', createUser)
 
+// Read One
 router.get('/:id', getOneUser)
 
+// Delete
 router.delete('/:id', deleteUser)
 
+// Update
 router.patch('/:id', updateUser)
 
 export default router;
