e6f2d040733bed2f4d13483f2b263142b62d8be0
Ali Kashev <kasev21@gmail.com>
2022-01-14 21:45:46 +0100

    added the rest of CRUD methodes + seperedet the controller

---
+++ controller/usersController.js
@@ -0,0 +1,42 @@
+import { v4 as uuidv4 } from 'uuid';
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
+let users = [];
+
+export const getUsers = (req, res) => {
+    console.log(users);
+    res.send(users);
+}
+
+export const getOneUser = (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser);
+}
+
+export const createUser = (req, res) => {
+    const user = req.body;
+    users.push({ ...user, id: uuidv4() });
+    res.send(`user with name ${user.firstName} added to the database`);
+}
+
+export const deleteUser = (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id != id);
+    res.send(`User with the ID: ${id} deleted from thet database.`);
+}
+
+export const updateUser = (req, res) => {
+    const { id } = req.params;
+    const {firstName, lastName, age} = req.body
+    const user = users.find((user) => user.id === id);
+
+    if (firstName) user.firstName = firstName;
+    if (lastName) user.lastName = lastName;
+    if (age) user.age = age;
+    
+
+    res.send(`User is updated`);
+}
--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,17 @@
 import express from "express";
-import { v4 as uuidv4 } from 'uuid';
-uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+import { getUsers, createUser, getOneUser, deleteUser, updateUser } from "../controller/usersController.js";
 
 const router = express.Router();
 
-let users = [];
-
 // all routes will start with /users
-router.get('/', (req, res) => {
-    console.log(users);
-    res.send(users);
-})
+router.get('/', getUsers)
 
-router.post('/', (req, res) => {
-    const user = req.body;
-    users.push({ ...user, id: uuidv4() });
-    res.send(`user with name ${user.firstName} added to the database`);
-})
+router.post('/', createUser)
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
+router.get('/:id', getOneUser)
 
-    const foundUser = users.find((user) => user.id === id);
-    res.send(foundUser);
-})
+router.delete('/:id', deleteUser)
 
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-
-    users = users.filter((user) => user.id != id);
-    res.send(`Use with the ID: ${id} deleted from thet database.`);
-})
+router.patch('/:id', updateUser)
 
 export default router;
